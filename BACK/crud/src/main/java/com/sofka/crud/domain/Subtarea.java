7cf5b6b8c749be57adf9e3c35cd29db6af3a96d4
Stevent Florez <steventflorez94@gmail.com>
2022-06-15 01:01:12 +0200

    primer commit

---
+++ BACK/crud/src/main/java/com/sofka/crud/domain/Subtarea.java
@@ -0,0 +1,19 @@
+package com.sofka.crud.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subtarea")
+public class Subtarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subtra_id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tarea_tra_id", nullable = false)
+    private Tarea tareaTra;
+
+
+
+}
---
+++ BACK/crud/src/main/java/com/sofka/crud/domain/Tarea.java
@@ -0,0 +1,23 @@
+package com.sofka.crud.domain;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tarea")
+public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tra_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "tra_titulo", nullable = false, length = 100)
+    private String traTitulo;
+
+    @OneToMany(mappedBy = "tareaTra")
+    private Set<Subtarea> subtareas = new LinkedHashSet<>();
+
+   
+
+}
